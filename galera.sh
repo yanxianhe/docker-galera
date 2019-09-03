dc79d66e4a4af23ec6851da8f9b86af4f2b4085e
yxh <yanxianhe@yapulan.com>
2019-09-03 20:26:12 +0800

    '修改 配置文件'

--- galera.sh
+++ galera.sh
@@ -20,45 +20,67 @@ if echo $* | grep -v -e '--skip-networking' | grep -v -e '--help'; then
   echo `getent hosts tasks.$SERVICE_NAME`
 
   if [ -n "$SERVICE_NAME" ]; then
-    CLUSTER_MEMBERS=`getent hosts tasks.$SERVICE_NAME | grep -v $IP_ADDRESS | awk '{print $1}'`
-    # Check we can see enough peers to form a Primary Component
-    if [ `getent hosts tasks.$SERVICE_NAME | wc -l` -lt $(((${CLUSTER_NODES}+1)/2)) ]; then
-      echo "Can't see enough peers to form a cluster; restarting."
-      exit 1
-    fi
-    # If we're the first node then bootstrap the cluster
-    if [ `getent hosts tasks.$SERVICE_NAME | sort -V | head -n 1 | awk '{print $1}'` = $IP_ADDRESS ]; then
-      echo "Looks like we're the first member. Testing for an established cluster between other nodes..."
-      # Check to see if the other nodes have an established cluster
-      for MEMBER in $CLUSTER_MEMBERS
-      do
-        echo "Testing $MEMBER..."
-        if echo "SHOW STATUS LIKE 'wsrep_local_state_comment';" | mysql -u root -p$MYSQL_ROOT_PASSWORD -h $MEMBER | grep "Synced"; then
-          # Connect to existing cluster
-          echo "Success! 😁"
-          export CLUSTER_ADDRESS="gcomm://$MEMBER?pc.wait_prim=yes"
-          break
-        else
-          echo "Failed 😫"
-        fi
-      done
-      # Can't connect to any other hosts; we need to bootstrap
-      if [ -z $CLUSTER_ADDRESS ]; then
-        echo "** No cluster found; bootstrapping on this node **"
-        export CLUSTER_ADDRESS="gcomm://"
+    while true ; do
+      CLUSTER_MEMBERS=`getent hosts tasks.$SERVICE_NAME | grep -v $IP_ADDRESS | awk '{print $1}'`
+      # Check we can see enough peers to form a Primary Component
+      if [ `getent hosts tasks.$SERVICE_NAME | wc -l` -ne ${CLUSTER_NODES} ]; then
+        echo "Can't see enough peers to form a cluster; restarting."
+        #exit 1
+          continue
       fi
-    fi
+      # If we're the first node then bootstrap the cluster
+      if [ `getent hosts tasks.$SERVICE_NAME | sort -V | head -n 1 | awk '{print $1}'` = $IP_ADDRESS ]; then
+        echo "Looks like we're the first member. Testing for an established cluster between other nodes..."
+        # Check to see if the other nodes have an established cluster
+        for MEMBER in $CLUSTER_MEMBERS
+        do
+          echo "Testing $MEMBER..."
+          if echo "SHOW STATUS LIKE 'wsrep_local_state_comment';" | mysql -u root -p$MYSQL_ROOT_PASSWORD -h $MEMBER | grep "Synced"; then
+            # Connect to existing cluster
+            echo "Success! 😁"
+            export CLUSTER_ADDRESS="gcomm://$MEMBER?pc.wait_prim=yes"
+            break
+          else
+            echo "Failed 😫"
+          fi
+        done
+        # Can't connect to any other hosts; we need to bootstrap
+        if [ -z $CLUSTER_ADDRESS ]; then
+          echo "** No cluster found; bootstrapping on this node **"
+          export CLUSTER_ADDRESS="gcomm://"
+        fi
+      fi
 
-    # Join existing cluster
-    if [ -z $CLUSTER_ADDRESS ]; then
-      # Fetch IPs of service members
-      CLUSTER_MEMBERS=`echo $CLUSTER_MEMBERS | tr ' ' ','`
-      export CLUSTER_ADDRESS="gcomm://$CLUSTER_MEMBERS?pc.wait_prim=yes"
-      export MYSQL_PWD="$MYSQL_ROOT_PASSWORD"
-      # Prevent entrypoint trying to (re)create the users
-      unset MYSQL_USER
-      export MYSQL_ROOT_HOST="localhost"
-    fi
+      # Join existing cluster
+      if [ -z $CLUSTER_ADDRESS ]; then
+        found=false
+          for MEMBER in $CLUSTER_MEMBERS
+          do
+            echo "Testing $MEMBER..."
+            if echo "SHOW STATUS LIKE 'wsrep_local_state_comment';" | mysql -u root -p$MYSQL_ROOT_PASSWORD -h $MEMBER | grep "Synced"; then
+              # Connect to existing cluster
+              echo "Success! 😁"
+              found=true
+              #export CLUSTER_ADDRESS="gcomm://$MEMBER?pc.wait_prim=yes"
+              break
+            else
+              echo "Failed 😫"
+            fi
+          done
+	  if ! $found ; then
+            continue
+          fi
+        # Fetch IPs of service members
+        CLUSTER_MEMBERS=`echo $CLUSTER_MEMBERS | tr ' ' ','`
+        export CLUSTER_ADDRESS="gcomm://$CLUSTER_MEMBERS?pc.wait_prim=yes"
+        export MYSQL_PWD="$MYSQL_ROOT_PASSWORD"
+        # Prevent entrypoint trying to (re)create the users
+        unset MYSQL_USER
+        #export MYSQL_ROOT_HOST="localhost"
+      fi
+
+      break
+    done
   fi
 
   echo "Cluster address is $CLUSTER_ADDRESS"
